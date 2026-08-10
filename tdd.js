15a8604631bc5cd9cf5d9b3c48537463ad0ab87e
tomiingm <tomasgigli@yahoo.com.ar>
2026-08-10 19:52:28 -0300

    Primer test en verde

--- tdd.js
+++ tdd.js
@@ -1,9 +1,17 @@
+const assert = require('assert');
+
+class Calculadora {
+    sumar(a, b) {
+        return 0;
+    }
+}
+
 function testSumarCeroMasCero() {
     numero1 = 0;
     numero2 = 0;
     const calc = new Calculadora();
     const resultado = calc.sumar(numero1, numero2);
-    assert.strictEquals(resultado, 0);
+    assert.strictEqual(resultado, 0);
 }
 
 testSumarCeroMasCero();

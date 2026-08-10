4e185dd144ea19c91f7835ec4a83dc44dcfc70ab
tomiingm <tomasgigli@yahoo.com.ar>
2026-08-10 20:15:05 -0300

    Primer refactoring

--- tdd.js
+++ tdd.js
@@ -2,17 +2,8 @@ const assert = require('assert');
 
 class Calculadora {
     sumar(numeros) {
-        const [a, b, c] = numeros.split(',').map(Number);
-        if (a === 0 && b === 0) {
-            return 0;
-    }
-        else if (a === 0 && b === 1) {
-            return 1;
-        }
-        else if (a === 1 && b === 1 && c === 1) {
-            return 3;
-        }
-    }
+    return numeros.split(',').map(Number).reduce((acc, n) => acc + n, 0);
+}
 }
 
 
